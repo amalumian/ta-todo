659e78c82ef50f620cdd4f698c6de9ed942f4659
Arthur Malumian <amalumian.dev@gmail.com>
2024-09-18 04:17:56 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import todoLogo from './assets/todo.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img style={{ width: '48px' }} src={todoLogo} alt="TODO logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
